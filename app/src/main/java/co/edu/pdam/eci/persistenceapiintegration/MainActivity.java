d9d3e11b36c4bdcfbad2eff795f4d7a24514e153
Santiago Carrillo <sancarbar@gmail.com>
2017-03-09 12:34:24 -0500

    Initial commit.

---
+++ app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/MainActivity.java
@@ -0,0 +1,16 @@
+package co.edu.pdam.eci.persistenceapiintegration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity
+    extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
